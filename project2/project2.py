e20a7642769fea3caa9a226198b5ed7d39164aab
huangjuite <huangjuite@gmail.com>
2020-11-06 15:14:29 +0800

    pr2

--- project2/project2.py
+++ project2/project2.py
@@ -1,10 +1,17 @@
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import math
 import pandas as pd
+
+
+def get_norm_magnitude(ft):
+    mg = np.abs(ft)
+    scale = 255/np.log(np.max(mg))
+    mg = scale*np.log(mg)
+    mg = np.clip(np.uint8(mg), 0, 255)
+    return mg
+
 
 # read image
 img = plt.imread('Bird 2.tif')
@@ -15,48 +22,59 @@ ftimage = np.fft.fft2(img)
 ftimage = np.fft.fftshift(ftimage)
 
 # magnitude of dtft
-magnitude = np.abs(ftimage)
-scale = 255/np.log(np.max(magnitude))
-magnitude = scale*np.log(magnitude)
-magnitude = np.clip(np.uint8(magnitude),0,255)
-cv2.imwrite('magnitude.tif', magnitude)
+magnitude = get_norm_magnitude(ftimage)
+cv2.imwrite('magnitude.png', magnitude)
 
 
 # ideal high, low pass filter
 rows, cols = img.shape
-crow, ccol = rows//2 , cols//2
+crow, ccol = rows//2, cols//2
 
 mask_low = np.zeros((rows, cols), np.uint8)
-mask_low[crow-30:crow+30, ccol-30:ccol+30] = 1
+for x in range(rows):
+    for y in range(cols):
+        dist = np.linalg.norm(np.array([x, y]) - np.array([crow, ccol]))
+        if dist < 30:
+            mask_low[x, y] = 1
 
-mask_high = np.ones((rows, cols), np.uint8)
-mask_high[crow-30:crow+30, ccol-30:ccol+30] = 0
+mask_high = 1 - mask_low
+
+cv2.imwrite('low_pass_mask.png', mask_low*255)
+cv2.imwrite('high_pass_mask.png', mask_high*255)
 
 # low pass image
-iftimage_low = ftimage*mask_low
-iftimage_low = np.fft.ifftshift(iftimage_low)
+ftimage_low_mask = ftimage*mask_low
+iftimage_low = np.fft.ifftshift(ftimage_low_mask)
 iftimage_low = np.fft.ifft2(iftimage_low)
-iftimage_low = np.uint8(np.abs(iftimage_low))
-cv2.imwrite('iftimage_low.tif', iftimage_low)
+iftimage_low = np.clip(np.uint8(np.abs(iftimage_low)), 0, 255)
+
+ftimage_low_mask = get_norm_magnitude(ftimage_low_mask)
+cv2.imwrite('ftimage_low.png', ftimage_low_mask)
+cv2.imwrite('iftimage_low.png', iftimage_low)
 
 # high pass image
-iftimage_high = ftimage*mask_high
-iftimage_high = np.fft.ifftshift(iftimage_high)
+ftimage_high_mask = ftimage*mask_high
+iftimage_high = np.fft.ifftshift(ftimage_high_mask)
 iftimage_high = np.fft.ifft2(iftimage_high)
-iftimage_high = np.uint8(np.abs(iftimage_high))
-cv2.imwrite('iftimage_high.tif', iftimage_high)
+iftimage_high = np.clip(np.uint8(np.abs(iftimage_high)), 0, 255)
+
+ftimage_high_mask = get_norm_magnitude(ftimage_high_mask)
+cv2.imwrite('ftimage_high.png', ftimage_high_mask)
+cv2.imwrite('iftimage_high.png', iftimage_high)
 
 # top 25 frequency
 magnitude = np.abs(ftimage)
 mag_l, _ = np.hsplit(magnitude, 2)
-mag = np.hstack((mag_l, np.zeros((512,256),dtype=np.uint8)))
+mag = np.hstack((mag_l, np.zeros((512, 256), dtype=np.uint8)))
+leftmag = get_norm_magnitude(mag)
+cv2.imwrite('magnitude_left.png', leftmag)
 
+print('\n #|    magnitude| (u, v)')
+print('============================')
 for i in range(25):
     idx = np.unravel_index(mag.argmax(), mag.shape)
     v = mag[idx]
     mag[idx] = 0
-    print("%02d, %.3f"%(i+1, v), idx)
+    print("%02d|"%(i+1), "%12.3f|"%(v), idx)
+print('============================')
 
-scale = 255/np.log(np.max(mag)+1)
-mag = scale*np.log(mag+1)
-cv2.imwrite('magnitude_left.tif', mag)
